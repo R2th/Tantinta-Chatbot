10466c3710b08eeb0ab999e17895d503d69bc77e
R2th <thangtruong1@1-thangtruong1-m.local>
2023-11-27 21:17:13 +0700

    clean code and add readme

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,44 +1,8 @@
 import Chat from "../pages/Chat";
-// import Home from "../pages/Home";
-import Login from "../pages/Login";
-import NotFoundPageError from "../pages/NotFoundPageError";
-// import User from "../pages/User";
-
-const AUTH_ROUTES = [
-  {
-    path: "login",
-    element: <Login />,
-  },
-];
 
 export const ROUTES = [
   {
     index: true,
     element: <Chat />,
-  },
-  {
-    path: "chat",
-    errorElement: <NotFoundPageError />,
-    children: [
-      {
-        path: ":sessionId",
-        element: <Chat />,
-      },
-    ],
-  },
-  // {
-  //   path: "user",
-  //   children: [
-  //     {
-  //       path: ":userId",
-  //       errorElement: <NotFoundPageError />,
-  //       element: <User />,
-  //     },
-  //   ],
-  // },
-
-  {
-    path: "auth",
-    children: AUTH_ROUTES,
   },
 ];
